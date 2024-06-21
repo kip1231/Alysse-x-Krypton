8d58adc868d936569976abb90a0c147b0770518b
kip1231 <81984493+kip1231@users.noreply.github.com>
2024-06-21 21:09:09 +0500

    Update EthosUI.lua

--- Costumers/EthosUI.lua
+++ Costumers/EthosUI.lua
@@ -1,3 +1,3 @@
-local scripok = game:HttpGet"https://raw.githubusercontent.com/KrutoySuslik/KraSus-Zet/main/Gui")
+local scripok = game:HttpGet"https://raw.githubusercontent.com/KrutoySuslik/KraSus-Zet/main/Gui"
 repeat task.wait() until game:IsLoaded()
 loadstring(scripok)()
